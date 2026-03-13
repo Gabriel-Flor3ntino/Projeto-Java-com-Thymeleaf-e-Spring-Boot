d51ec9988edeb38257c7882a171d4517197f234f
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-03-13 12:17:36 -0300

    Criando Projeto Spring Boot com Thymeleaf Usando Spring Initializr

---
+++ src/main/java/com/br/odevjava/formulario/AulaDeCriacaoDeFormularioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.br.odevjava.formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDeCriacaoDeFormularioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDeCriacaoDeFormularioSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/odevjava/formulario/AulaDeCriacaoDeFormularioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.odevjava.formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaDeCriacaoDeFormularioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
